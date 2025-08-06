75e3e4b6a2c507cdc582623f64834034ba221a3f
magmostafa <mag@taippa.com>
2025-08-07 01:39:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)               # Adds /app to sys.path
+sys.path.append(os.path.join(current_dir, "taippa"))  # Adds /app/taippa
+
 from taippa.main import app
